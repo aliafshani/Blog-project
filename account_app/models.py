33f731b3e0a10edad3679843771bd2457920f3ad
alish <alifshania222@gmail.com>
2023-09-04 00:53:43 -0700

    build profile onotoone

--- account_app/models.py
+++ account_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+
+class profile(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    father_name=models.CharField(max_length=12)
+    meli_code=models.CharField(max_length=12)
+    image=models.ImageField(upload_to='media/photo')
+
+    def __str__(self):
+        return self.user.username
